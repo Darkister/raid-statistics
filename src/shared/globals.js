abb80d3a523aedf5ff2c13b439534b02a1350d21
Darkister <marco.schneller92@gmail.com>
2025-01-09 16:14:02 +0100

    update version

--- src/shared/globals.js
+++ src/shared/globals.js
@@ -1,4 +1,4 @@
-var scriptVersion = "0.1.1",
+var scriptVersion = "0.2.0",
   ss = SpreadsheetApp.getActiveSpreadsheet(),
   settingsSheet = ss.getSheetByName("Settings"),
   logSheet = ss.getSheetByName("Logs"),
